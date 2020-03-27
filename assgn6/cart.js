53bb5c6417ac3184a286f6ccd12fe8301012d753
kristelgrace <60626354+kristelgrace@users.noreply.github.com>
2020-03-26 23:51:35 -0400

    Added wishlist function and fixed bugs

--- assgn6/cart.js
+++ assgn6/cart.js
@@ -73,6 +73,63 @@ function getOrder() {
 	}
 }
 
+function addOrder () {
+  var originalGlaze = document.getElementById("glaze"); // retrieve the glaze options
+	var selectedGlaze = originalGlaze.options[originalGlaze.selectedIndex].text; //retrieve selected glaze
+	var glazeImage = "none.png" // set a sample image
+
+  //update the selected image
+	if (selectedGlaze=="none")
+	{
+		glazeImage="none.png";
+	}
+	else if (selectedGlaze=="vanilla-milk")
+	{
+		glazeImage="vanilla.png";
+	}
+	else if (selectedGlaze=="sugar-milk")
+	{
+		glazeImage="sugar.png";
+	}
+	else if (selectedGlaze=="double-chocolate")
+	{
+		glazeImage="choco.png";
+	}
+
+	var originalRoll = document.getElementById("roll"); // retrieve the roll options
+	var selectedRoll = originalRoll.options[originalRoll.selectedIndex].text; //retrieve selected roll
+
+	var originalBox = document.getElementById("boxOrder"); // retrieve the box options
+	var selectedBox = originalBox.value; // retrieve the entered box quantity
+
+	var currentPrice = document.getElementById("priceOriginal").textContent // retrieve the text displayed on page
+
+	var itemsInWish = document.getElementById("wishCount").textContent; //retrieve items in bag count
+	itemsInWish = parseInt(itemsInWish)+1; // add by one when add to cart is clicked
+  sessionStorage.setObj("itemsInWish", itemsInWish); //set item count in storage
+  console.log(itemsInWish)
+  document.getElementById("wishCount").textContent = itemsInWish; // set the displayed item count to new item count
+  sessionStorage.setObj("itemsInWish", itemsInWish); //set item count in storage
+
+	var itemAdded = new Original(selectedGlaze, selectedRoll, selectedBox, glazeImage, currentPrice); //create new object
+
+  // adding the newest item to the array of items
+	if (itemsInWish == 1)
+	{
+		var wishItems = new Array(itemAdded); //create new array if first item
+		sessionStorage.setObj("wishItems", wishItems); //save the array
+    console.log(itemAdded)
+    console.log(wishItems)
+	}
+	else if (itemsInWish >= 2)
+	{
+		var wishItems = sessionStorage.getObj("wishItems") //retrieve array for more than one item in bag
+		wishItems.push(itemAdded); //add newest item to array
+		sessionStorage.setObj("wishItems", wishItems); //save the new array
+    console.log(itemAdded)
+    console.log(wishItems)
+	}
+}
 
 // change picture when selecting different glazes
 function updateGlaze() {
@@ -243,20 +300,67 @@ function removeItem () {
             document.getElementsByClassName("bag-price")[0].textContent = "$" + subtotal + ".00" //update price display
             document.getElementsByClassName("bag-price")[1].textContent = "$" + subtotal + ".00" //update price display
           }
+        if (parseInt(itemsInBag)==0)
+        {
+          document.getElementsByClassName("bag-price")[0].textContent = "$0.00"
+          document.getElementsByClassName("bag-price")[1].textContent = "$0.00"
+        }
         }
       }))
     }
   })
 }
 
+function removeWish () {
+  var wishItems = sessionStorage.getObj("wishItems"); // retrieve cart items
+  var removeIndex = document.querySelectorAll('.removeWish');
+  var allWishes = document.querySelectorAll('.wish-1');
+  var itemsInWish = document.getElementById("wishCount").textContent; //retrieve items in bag count
+
+  // listen to check if the "x" is clicked, sourced from w3schools and stackoverflow
+  removeIndex.forEach((item) => {
+    {
+      item.addEventListener('click', ((j) =>
+      {
+        var index = Array.prototype.indexOf.call(removeIndex, item);
+        wishItems.splice(index, 1);
+
+        if (parseInt(itemsInWish) == 1)
+        {
+          // change text to show that there are no items in the cart
+          allWishes.item(index).innerHTML = "there are no items in your wishlist!" + "<br>" + "visit a store or check our menu to start ordering online";
+      		allWishes.item(index).style.display = "block";
+      		allWishes.item(index).style.textAlign = "center";
+        }
+        else if (parseInt(itemsInWish) >= 2)
+        {
+          // remove the box of the item clicked on
+          allWishes.item(index).remove();
+        }
+
+        sessionStorage.setObj("wishItems", wishItems); // save the new cart
+        sessionStorage.setObj("allWishes", allWishes); // save the updated display
+      	itemsInWish = parseInt(itemsInWish)-1; // add by one when add to cart is clicked
+      	document.getElementById("wishCount").textContent = itemsInWish; // set the displayed item count to new item count
+        sessionStorage.setObj("itemsInWish", itemsInWish); //set item count in storage
+      }))
+    }
+  })
+}
 
 function onLoad () {
 	var checkCount = sessionStorage.getItem("itemsInBag"); //retrieve stored count of items in bag
   var subtotal = 0
+  var displayedPrice = document.getElementsByClassName("bag-price")[0]
 
 	if (checkCount==undefined)
 	{
 		checkCount = 0;
+    if(displayedPrice !== undefined)
+    {
+      document.getElementsByClassName("bag-price")[0].textContent = "$0.00"
+      document.getElementsByClassName("bag-price")[1].textContent = "$0.00"
+    }
 	}
 	document.getElementById("itemCount").textContent = checkCount; //set displayed count to stored count
 
@@ -269,10 +373,11 @@ function onLoad () {
 		itemOne.style.textAlign = "center";
 	}
 
-  var cartItems = sessionStorage.getObj("cartItems")
-  if (cartItems !== null && cartItems.length > 1)
+  var boxItem = document.getElementsByClassName("all-items")[0];
+  var cartItems = sessionStorage.getObj("cartItems");
+  if (cartItems !== null && cartItems.length > 1 && boxItem !== undefined)
   {
-    var cartItems = sessionStorage.getObj("cartItems", cartItems);
+    var cartItems = sessionStorage.getObj("cartItems");
     for (var i = 0; i < (cartItems.length-1); i++)
     {
       let boxes = document.getElementsByClassName("all-items")[0];
@@ -379,4 +484,98 @@ function onLoad () {
           document.getElementsByClassName("bag-price")[1].textContent = "$" + subtotal + ".00" //update price display
         }
       }
-}
+
+    var checkWish = sessionStorage.getItem("itemsInWish"); //retrieve stored count of items in bag
+
+  	if (checkWish==undefined)
+  	{
+  		checkWish = 0;
+  	}
+  	document.getElementById("wishCount").textContent = checkWish; //set displayed count to stored count
+
+  	var wishOne = document.getElementsByClassName("wish-1")[0]; //get all elements within the class item-1
+    //change the display of cart items when there are no items in cart
+  	if (wishOne !== undefined && checkWish<1)
+  	{
+  		wishOne.innerHTML = "there are no items in your wishlist!" + "<br>" + "visit a store or check our menu to start ordering online";
+  		wishOne.style.display = "block";
+  		wishOne.style.textAlign = "center";
+  	}
+
+    var boxWish = document.getElementsByClassName("all-wishes")[0];
+    var wishItems = sessionStorage.getObj("wishItems");
+    if (wishItems !== null && wishItems.length > 1 && boxWish !== undefined)
+    {
+      for (var i = 0; i < (wishItems.length-1); i++)
+      {
+        let wishboxes = document.getElementsByClassName("wish-items")[0];
+        let wishclone = document.getElementsByClassName("wish-1")[0];
+        let newWishClone = wishclone.cloneNode([true]);
+        wishboxes.appendChild(newWishClone);
+      }
+    }
+
+    if (wishItems !== null && wishItems!== 0)
+    {
+      removeWish()
+      for (var i = 0; i < wishItems.length; i++)
+      {
+        var wishGlaze = document.getElementsByClassName("wishedGlaze")[i]; //get the glaze options in cart page
+
+        	if (wishGlaze !== null && wishGlaze!== undefined)
+        	{
+        		var itemsInWish = sessionStorage.getObj("wishItems"); //retrieve the array of items in cart
+            var displayGlaze = itemsInWish[i].glaze; //retrieve the glaze in first item
+            document.getElementsByClassName("wishedGlaze")[i].textContent = displayGlaze
+
+            var currentPicture = document.getElementsByClassName("wishImage")[i]; // retrieve the small image in cart
+
+            // update glaze display index and picture based on cart selection
+        		if (displayGlaze == "none")
+        		{
+        			currentPicture.src = "smallnone.png";
+        		}
+        		else if (displayGlaze == "sugar-milk")
+        		{
+        				currentPicture.src = "smallsugar.png"
+        		}
+        		else if (displayGlaze == "vanilla-milk")
+        		{
+        				currentPicture.src = "smalloriginal.png";
+        		}
+        		else if (displayGlaze == "double-chocolate")
+        		{
+        				currentPicture.src = "smallchoco.png";
+        		}
+        	}
+
+        	var wishRoll = document.getElementsByClassName("wishedRolls")[i]; //retrieve the roll in first item
+
+        	if (wishRoll !== null && wishRoll !== undefined)
+        	{
+            var itemsInWish = sessionStorage.getObj("wishItems"); //retrieve the array of items in cart
+            var displayRolls = itemsInWish[i].roll; //retrieve the rolls in first item
+            document.getElementsByClassName("wishedRolls")[i].textContent = displayRolls
+        	}
+
+          var wishBoxes = document.getElementsByClassName("wishedBoxes")[i]; //retrieve the boxes in first item
+
+        	if (wishBoxes !== null && wishBoxes !== undefined)
+        	{
+            var itemsInWish = sessionStorage.getObj("wishItems"); //retrieve the array of items in cart
+            var displayBoxes = itemsInWish[i].box; //retrieve the boxes in first item
+            document.getElementsByClassName("wishedBoxes")[i].textContent = displayBoxes
+        	}
+
+          var wishPrice = document.getElementsByClassName("wishedPrice")[i]; //retrieve the price in first item
+
+          if (wishPrice !== null && wishPrice !== undefined)
+        	{
+            var itemsInWish = sessionStorage.getObj("wishItems"); //retrieve the array of items in cart
+            var displayPrice = itemsInWish[i].price; //retrieve the price in first item
+            document.getElementsByClassName("wishedPrice")[i].textContent = displayPrice
+        	}
+
+        }
+      }
+  }
